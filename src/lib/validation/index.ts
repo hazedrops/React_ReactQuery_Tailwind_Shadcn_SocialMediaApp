8206c3f43342e417596d5eefd57c5b3a704d33a6
hazedrops <hazedrops@hotmail.com>
2024-03-23 21:31:09 -0700

    Create a new account form and loader is added

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod'
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: 'Too short' }), 
+  username: z.string().min(2, { message: 'Too short' }),
+  email: z.string().email(),
+  password: z.string().min(8, { message: 'Password must be at least 8 characters.' }),
 })
